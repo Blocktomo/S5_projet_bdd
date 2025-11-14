cbfca45964eeb8ff694188915eb3141f8f05399a
tbeverly01 <tbeverly01@SEC-C210-F14.csip.in>
2025-11-14 09:03:48 +0100

    ajout de connexion bdd thomas dans readme

--- src/main/java/fr/insa/toto/model/ReadMe.java
+++ src/main/java/fr/insa/toto/model/ReadMe.java
@@ -7,6 +7,10 @@ https://github.com/Blocktomo/S5_projet_bdd.git
 token2
 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
 
+connexion bdd thomas : 
+92.222.25.165
+m3_tbeverly01
+71884255
  */
 package fr.insa.toto.model;
 
